c0cacb019373132a0b32a7376d6db68ff259fb68
DarkQuys <quockp2@gmail.com>
2024-08-19 16:37:17 +0700

    Edit varible name

--- FeeColector.sol
+++ FeeColector.sol
@@ -1,12 +1,12 @@
-pragma solidity ^0.8.19;
+pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/access/AccessControl.sol" ;
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract FeeColector is Ownable(msg.sender) , AccessControl{
     bytes32 public constant ADMINER = keccak256("ADMINER");
-    uint256 public PlatformFee ;
-    uint256 public RegistrationFee ;
-    event updatedPlatformFee(uint256 PlatformFee) ;
-    event updatedRegistrationFee(uint256 RegistrationFee) ;
+    uint256 public platformFee ;
+    uint256 public registrationFee ;
+    event updatedPlatformFee(uint256 platformFee) ;
+    event updatedRegistrationFee(uint256 registrationFee) ;
     constructor() Ownable(msg.sender) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMINER, msg.sender);
@@ -15,15 +15,15 @@ contract FeeColector is Ownable(msg.sender) , AccessControl{
         require(hasRole(ADMINER, msg.sender), "AccessControl: account is missing ADMINER");
         _;
     }
-    function setPlatformFee(uint256 _PlatformFee) public onlyAdmin{
-        require(_PlatformFee > 0 , "_PlatformFee must greater than 0");
-        PlatformFee = _PlatformFee ;
-        emit updatedPlatformFee(_PlatformFee);
+    function setPlatformFee(uint256 _platformFee) public onlyAdmin{
+        require(_platformFee > 0 , "_platformFee must greater than 0");
+        platformFee = _platformFee ;
+        emit updatedPlatformFee(_platformFee);
     }
-    function setRegistrationFee(uint256 _RegistrationFee) public onlyAdmin {
-        require(_RegistrationFee > 0 , "__RegistrationFee must greater than 0");
-        RegistrationFee = _RegistrationFee ;
-        emit updatedRegistrationFee(_RegistrationFee);
+    function setRegistrationFee(uint256 _registrationFee) public onlyAdmin {
+        require(_registrationFee > 0 , "__registrationFee must greater than 0");
+        registrationFee = _registrationFee ;
+        emit updatedRegistrationFee(_registrationFee);
     }
     function addAdmin(address account ) public onlyOwner {
         grantRole(ADMINER, account) ;

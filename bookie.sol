ef58c28b991ca9301e09e8203e0ae4ac7340d925
Giang Văn Đạt <datcay658@gmail.com>
2024-08-19 15:56:26 +0700

    update

--- bookie.sol
+++ bookie.sol
@@ -58,20 +58,21 @@ contract Bookie is AccessControl,Ownable(msg.sender){
       bool _status
     );
 
-    constructor(){
+    constructor(
+        address _feeConlecter
+    ){
+        feeConlecter = FeeConlecter(_feeConlecter);
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ADMIN_ROLE, msg.sender);
 
         withdrawWallet = payable(msg.sender);
-        feeConlecter = new FeeConlecter();
     }
 
-    function createPlayerOrArbiter(string memory _username, uint8 _typeplayer) public payable {
+    function createPlayer(string memory _username, uint8 _typeplayer) public payable {
         uint256 _registrationFee = feeConlecter.registrationFee();
         require(msg.sender != address(0),"Wallet does not exist");
         require(bytes(_username).length != 0,"Empty username");
         require(_typeplayer>=0 && _typeplayer < 2 ,"Empty type");
-        require(_registrationFee>0, "Fee must be greater than 0");
         require(_registrationFee==msg.value,"Fee is not enough");
 
         withdrawWallet.transfer(_registrationFee);
@@ -80,49 +81,50 @@ contract Bookie is AccessControl,Ownable(msg.sender){
         countPlayer++;
 
         emit CreatePlayer(_username, msg.sender, _registrationFee, _typeplayer);
-
-        if(_typeplayer == uint8(TypePlayer.arbiter)){
-             _grantRole(ARBITER_ROLE, msg.sender);
-            listArbiter[countArbiter] = arbiter(msg.sender,true);
-            countArbiter++;
-
-            emit CreateArbiter(msg.sender);
-        }
         
     }
 
-    // function createArbiter(address _wallet) public onlyRole(ADMIN_ROLE){
-    //     require(_wallet != address(0),"Address does not exist");
-    //     _grantRole(ARBITER_ROLE, _wallet);
-    //     listArbiter[countArbiter] = arbiter(_wallet,true);
-    //     countArbiter++;
+    function createArbiter(address _wallet) public onlyRole(ADMIN_ROLE){
+        require(_wallet != address(0),"Address does not exist");
+        _grantRole(ARBITER_ROLE, _wallet);
+        listArbiter[countArbiter] = arbiter(_wallet,true);
+        countArbiter++;
 
-    //     emit CreateArbiter(_wallet);
-    // }
+        emit CreateArbiter(_wallet);
+    }
 
 
-     function updatePlayer(uint128 _countPlayer, string memory _username, uint8 _typeplayer) public{
-        require(listPlayer[_countPlayer].wallet != address(0),"Player does not exist");
-        require(msg.sender != address(0),"Wallet does not exist");
+     function updatePlayer(uint128 _count, address _wallet, string memory _username, uint8 _typeplayer, bool _status) public onlyRole(ADMIN_ROLE){
+        require(_wallet != address(0),"Wallet does not exist");
         require(bytes(_username).length != 0,"Empty username");
         require(_typeplayer>=0 && _typeplayer < 3 ,"Empty type");
 
-        listPlayer[_countPlayer].wallet = msg.sender;
-        listPlayer[_countPlayer].username = _username;
-        listPlayer[_countPlayer].typePlayer = _typeplayer;
+        if(_typeplayer==uint8(TypePlayer.player)){
+            require(listPlayer[_count].wallet != address(0),"Player does not exist");
+            listPlayer[_count].wallet = _wallet;
+            listPlayer[_count].username = _username;
+            listPlayer[_count].typePlayer = _typeplayer;
 
-        emit UpdatePlayer(_countPlayer, _username, msg.sender, _typeplayer);
+            emit UpdatePlayer(_count, _username, _wallet, _typeplayer);
+        }else{
+            require(listArbiter[_count].wallet != address(0),"Player does not exist");
+            listArbiter[_count].status = _status;
+
+            emit UpdateArbiter(_count, _wallet , _status);
+        }
+
+        
     }
 
-     function updateArbiter(uint _countArbiter, address _wallet, bool _status) public onlyRole(ADMIN_ROLE){
-        require(listArbiter[_countArbiter].wallet != address(0),"Player does not exist");
-        require(_wallet != address(0),"Address does not exist");
+    //  function updateArbiter(uint _countArbiter, address _wallet, bool _status) public onlyRole(ADMIN_ROLE){
+    //     require(listArbiter[_countArbiter].wallet != address(0),"Player does not exist");
+    //     require(_wallet != address(0),"Address does not exist");
 
-        listArbiter[_countArbiter].wallet = _wallet;
-        listArbiter[_countArbiter].status = _status;
+    //     listArbiter[_countArbiter].wallet = _wallet;
+    //     listArbiter[_countArbiter].status = _status;
 
-        emit UpdateArbiter(_countArbiter, _wallet, _status);
-    }
+    //     emit UpdateArbiter(_countArbiter, _wallet, _status);
+    // }
 
-      function deposit() public payable {}
+    function deposit() public payable {}
 }

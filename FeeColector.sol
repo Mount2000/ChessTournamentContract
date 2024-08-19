fcad018c53b3b8a029ccffb29ac4d47718bd9f1b
DarkQuys <quockp2@gmail.com>
2024-08-19 16:49:43 +0700

    Edit ADMIN_ROLE name

--- FeeColector.sol
+++ FeeColector.sol
@@ -2,17 +2,17 @@ pragma solidity >=0.8.2 <0.9.0;
 import "@openzeppelin/contracts/access/AccessControl.sol" ;
 import "@openzeppelin/contracts/access/Ownable.sol";
 contract FeeColector is Ownable(msg.sender) , AccessControl{
-    bytes32 public constant ADMINER = keccak256("ADMINER");
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     uint256 public platformFee ;
     uint256 public registrationFee ;
     event updatedPlatformFee(uint256 platformFee) ;
     event updatedRegistrationFee(uint256 registrationFee) ;
-    constructor() Ownable(msg.sender) {
+    constructor(){
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(ADMINER, msg.sender);
+        _grantRole(ADMIN_ROLE, msg.sender);
     }
     modifier onlyAdmin() {
-        require(hasRole(ADMINER, msg.sender), "AccessControl: account is missing ADMINER");
+        require(hasRole(ADMIN_ROLE, msg.sender), "AccessControl: account is missing ADMINER");
         _;
     }
     function setPlatformFee(uint256 _platformFee) public onlyAdmin{
@@ -26,6 +26,6 @@ contract FeeColector is Ownable(msg.sender) , AccessControl{
         emit updatedRegistrationFee(_registrationFee);
     }
     function addAdmin(address account ) public onlyOwner {
-        grantRole(ADMINER, account) ;
+        grantRole(ADMIN_ROLE, account) ;
     }
 } 
